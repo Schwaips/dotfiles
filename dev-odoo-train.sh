065b08a030d46cdd65617fa711457df8fdd9665c
Schwaips <charlie.bertrand@live.com>
2023-10-30 11:56:10 +0100

    odoo training sheel

---
+++ dev-odoo-train.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+tmux send-keys "cd ~/code/Schwaips/drakkar/odoo_folder && ./odoo-bin --addons-path="addons/,../enterprise/,../odoo-technical-training" -d charliebertrand -u estate  --dev xml" C-m
+tmux split-window -h
+tmux send-keys "cd ~/code/Schwaips/drakkar/odoo_folder" C-m
+tmux split-window -v
+tmux send-keys "cd ~/code/Schwaips/drakkar/odoo_folder/odoo-technical-training" C-m
+tmux select-pane -t 1
+# tmux split-window -v
+# tmux send-keys "cd ~/code/Schwaips/drakkar/Igere/file-server && yarn dev" C-m
+# tmux split-window -v
+# tmux send-keys "ngrok http 3000 --subdomain=ivan-charlie" C-m
+# tmux resize-pane -t 3 -D 10
