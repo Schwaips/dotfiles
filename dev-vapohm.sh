f5504b75e2dd53aec4d1362fedfe89c9701eeb35
Schwaips <charlie.bertrand@live.com>
2023-11-02 16:52:55 +0100

    dotfiles

---
+++ dev-vapohm.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+tmux send-keys "cd ~/code/Schwaips/drakkar/odoo_folder/odoo && odoo-bin --addons-path="addons,../enterprise,../Vap-Ohm" -d vapohm2 -u pos_sales_history_shortcut --dev xml" C-m
+tmux split-window -h
+tmux send-keys "cd ~/code/Schwaips/drakkar/odoo_folder" C-m
+tmux split-window -v
+tmux send-keys "cd ~/code/Schwaips/drakkar/Vap-Ohm" C-m
+tmux select-pane -t 1
