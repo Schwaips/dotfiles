eead0a2e72faa44f11f7ed26dc58cf82d2ac224b
Schwaips <charlie.bertrand@live.com>
2023-12-21 11:54:26 +0100

    Dote file updated with new script for odoo vapohm

--- dev-vapohm.sh
+++ dev-vapohm.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
-tmux send-keys "cd ~/code/Schwaips/drakkar/odoo_folder/odoo && ./odoo-bin --addons-path="addons,../enterprise,../Vap-Ohm" -d vapohm2 -u pos_search_drakkar --dev xml" C-m
+MODULE_NAME=$1
+
+
+function usage() {
+  echo " ERR -- Veuillez spécifier le nom du module à updgrade pour odoo"
+  echo "Exemple devodoovapohm <my_module>"
+  echo "Usage: $0 [MODULE_NAME]"
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]
+then
+  usage
+  exit 0
+fi
+
+if [ -z "$MODULE_NAME" ]
+then
+  usage
+  exit 1
+fi
+
+
+tmux send-keys "cd ~/code/Schwaips/drakkar/odoo_folder/odoo && ./odoo-bin --addons-path="addons,../enterprise,../Vap-Ohm" -d vapohm2 -u $MODULE_NAME --dev xml" C-m
 tmux split-window -h
 tmux send-keys "cd ~/code/Schwaips/drakkar/odoo_folder" C-m
 tmux split-window -v
